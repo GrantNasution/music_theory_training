d7a582e9bfb05b9437cada13c89214605302569f
Grant <honeyhomie@gmail.com>
2020-09-04 20:55:48 -0700

    added components folder

---
+++ src/components/welcomepage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class WelcomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>MUSIC THEORY TRAINING</h1>
+                <div>
+                    <ul>
+                        <li>Ear Training</li>
+                        <li>Note ID</li>
+                        <li>Interval ID</li>
+                        <li>Chord ID</li>
+                        <li>test</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default WelcomePage;
